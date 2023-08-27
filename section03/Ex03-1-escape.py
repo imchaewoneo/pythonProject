c2e24e60288a437b3143bd4723162158566ae5be
im2chae1 <lcw1880@naver.com>
2023-08-27 17:42:09 +0900

    ex03

---
+++ section03/Ex03-1-escape.py
@@ -0,0 +1,16 @@
+'''
+이스케이프 문자
+    \t -> 탭
+    \b -> 백스페이스
+    \\ -> \
+    \' -> 작은따옴표
+    \" -> 큰따옴표
+    \n -> 개행(줄바꿈)
+'''
+print('Hello \'World\'')
+print("Hello \"World\"")
+print('Hello "World"')
+print('*\n**\n***')
+print('이름\t연락처\t주소')
+print('제시카\t02-1234-5678')
+
